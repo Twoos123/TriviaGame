41ab221f18470a153b067841ccb81c8d970a3e3e
Asad Ali <masadbali190@gmail.com>
2023-09-24 12:12:58 -0400

    Updated App.cs and Trivia.js
    added animations and changed a few UI elements

--- README.md
+++ README.md
@@ -1,70 +1,25 @@
-# Getting Started with Create React App
+This is my submission for the OutStem front-end challenge
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Goals achieved:
 
-## Available Scripts
+Goal 1
+Fetch an easy, true/false, general knowledge question from the API, and print the response to the console
 
-In the project directory, you can run:
+Goal 2
+Instead of printing to the console, display the question to the user, and allow them to select between the true and false options using whichever UI elements you think are best suited.
 
-### `npm start`
+Goal 3
+If the user choses the correct answer, give them a success message, and the option to move on to the next stage of the game.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+If they chose the incorrect answer, give them a failure message including what the correct answer was, and have them restart the game.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Goal 4
+Now fetch an easy multiple choice question from the general knowledge category.
 
-### `npm test`
+Display the question on the screen for the user, and allow them to select their answer with whichever UI elements you think are best suited
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Goal 5
+If the user choses the correct answer, give them a success message, and the option to move on to the next stage of the game.
 
-### `npm run build`
+If they chose the incorrect answer, give them a failure message including what the correct answer was, and have them restart the game.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/Trivia.js
+++ src/Trivia.js
@@ -1,63 +1,97 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import he from 'he'; // Import the 'he' library for decoding HTML entities
+import './App.css'; 
 
 const Trivia = () => {
   const [question, setQuestion] = useState('');
-  const [userAnswer, setUserAnswer] = useState(null); // Store user's answer (true or false)
-  const [feedback, setFeedback] = useState(''); // Display success or failure message
+  const [userAnswer, setUserAnswer] = useState(null);
+  const [feedback, setFeedback] = useState('');
+  const [showNextButton, setShowNextButton] = useState(false); // Control visibility of "Next" button
+  const [choices, setChoices] = useState([]); // Store multiple-choice options
+  const [correctAnswer, setCorrectAnswer] = useState(''); // Store the correct answer
+
+  const fetchQuestion = async () => {
+    try {
+      const response = await axios.get(
+        'https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple&category=9'
+      );
+
+      if (response.data.results.length > 0) {
+        const fetchedQuestion = he.decode(response.data.results[0].question); // Decode HTML-encoded characters
+        const fetchedChoices = response.data.results[0].incorrect_answers;
+        const correct = response.data.results[0].correct_answer;
+
+        // Shuffle the choices and include the correct answer
+        const shuffledChoices = [...fetchedChoices, correct].sort(() => Math.random() - 0.5);
+
+        setQuestion(fetchedQuestion);
+        setChoices(shuffledChoices);
+        setCorrectAnswer(correct);
+
+        // Reset userAnswer and feedback when a new question is fetched
+        setUserAnswer(null);
+        setFeedback('');
+        setShowNextButton(false);
+      } else {
+        console.log('No questions found.');
+      }
+    } catch (error) {
+      console.error('Error fetching question:', error);
+    }
+  };
 
   useEffect(() => {
-    // Function to fetch a question from the API
-    const fetchQuestion = async () => {
-      try {
-        const response = await axios.get(
-          'https://opentdb.com/api.php?amount=1&difficulty=easy&type=boolean&category=9'
-        );
-
-        if (response.data.results.length > 0) {
-          const fetchedQuestion = response.data.results[0].question;
-          setQuestion(fetchedQuestion);
-        } else {
-          console.log('No questions found.');
-        }
-      } catch (error) {
-        console.error('Error fetching question:', error);
-      }
-    };
-
-    // Call the fetchQuestion function when the component mounts
-    fetchQuestion();
+    fetchQuestion(); // Fetch the initial question when the component mounts
   }, []);
 
-  // Function to handle user's answer
   const handleAnswer = (answer) => {
-    // Check if the user's answer is correct
-    const isCorrect = answer === 'true'; // You may need to adjust this based on the API response format
+    const isCorrect = answer === correctAnswer;
 
-    // Provide feedback to the user
     if (isCorrect) {
-      setFeedback('Correct!'); // Display success message
+      setFeedback('Correct!');
+      setShowNextButton(true); // Show the "Next" button when the answer is correct
     } else {
-      setFeedback('Incorrect!'); // Display failure message
+      setFeedback(`Incorrect. The correct answer is ${correctAnswer}.`); // Provide the correct answer
+      setShowNextButton(false); // Hide the "Next" button for incorrect answers
     }
 
-    // Store the user's answer
     setUserAnswer(answer);
   };
 
+  const handleNextQuestion = () => {
+    fetchQuestion(); // Fetch the next question
+  };
+
+  const handleRestart = () => {
+    fetchQuestion(); // Fetch the initial question to restart the game
+  };
+
   return (
-    <div>
+    <div className="container">
       <h1>Trivia Game</h1>
-      <p>Question: {question}</p>
-
-      {/* Display answer options */}
-      <div>
-        <button onClick={() => handleAnswer('true')}>True</button>
-        <button onClick={() => handleAnswer('false')}>False</button>
+      <p className="question">Question: {question}</p>
+  
+      <div className="choices">
+        {choices.map((choice, index) => (
+          <button key={index} onClick={() => handleAnswer(choice)}>
+            {choice}
+          </button>
+        ))}
       </div>
-
-      {/* Display feedback */}
-      {feedback && <p>{feedback}</p>}
+  
+      {feedback && <p className={userAnswer === correctAnswer ? 'feedback success' : 'feedback'}>
+        {feedback}
+      </p>}
+  
+      {showNextButton && <div className="button-container">
+        <button className="next" onClick={handleNextQuestion}>Next</button>
+      </div>}
+      {!showNextButton && userAnswer !== null && (
+        <div className="button-container">
+        <button className="restart" onClick={handleRestart}> Restart</button>
+  </div>
+)}
     </div>
   );
 };
