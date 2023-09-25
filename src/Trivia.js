fa6c982a951a4c7c8c0c1830ef0162d945728e31
Asad Ali <masadbali190@gmail.com>
2023-09-24 21:52:54 -0400

    Updated code

--- src/Trivia.js
+++ src/Trivia.js
@@ -1,37 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import he from 'he'; // Import the 'he' library for decoding HTML entities
+import he from 'he';
 import './App.css';
 
 const Trivia = () => {
   const [question, setQuestion] = useState('');
   const [userAnswer, setUserAnswer] = useState(null);
   const [feedback, setFeedback] = useState('');
-  const [showNextButton, setShowNextButton] = useState(false); // Control visibility of "Next" button
-  const [choices, setChoices] = useState([]); // Store multiple-choice options
-  const [correctAnswer, setCorrectAnswer] = useState(''); // Store the correct answer
-  const [difficulty, setDifficulty] = useState('easy'); // Default to 'easy'
-  const [correctAnswersCount, setCorrectAnswersCount] = useState(0); // Track correct answers count
+  const [showNextButton, setShowNextButton] = useState(false);
+  const [choices, setChoices] = useState([]);
+  const [correctAnswer, setCorrectAnswer] = useState('');
+  const [difficulty, setDifficulty] = useState('easy');
+  const [category, setCategory] = useState(9);
+  const [lives, setLives] = useState(3);
+  const [points, setPoints] = useState(0);
+  const [buttonsDisabled, setButtonsDisabled] = useState(false);
+
+  const difficultyOptions = ['easy', 'medium', 'hard'];
+  const categoryOptions = [
+    { id: 9, name: 'General Knowledge' },
+    { id: 11, name: 'Film' },
+    { id: 12, name: 'Music' },
+  ];
 
   const fetchQuestion = async () => {
+    setButtonsDisabled(false); // Enable buttons for the next question
     try {
       const response = await axios.get(
-        `https://opentdb.com/api.php?amount=1&difficulty=${difficulty}&type=multiple&category=9`
+        `https://opentdb.com/api.php?amount=1&difficulty=${difficulty}&type=multiple&category=${category}`
       );
 
-      if (response.data.results.length > 0) {
-        const fetchedQuestion = he.decode(response.data.results[0].question); // Decode HTML-encoded characters
-        const fetchedChoices = response.data.results[0].incorrect_answers;
-        const correct = response.data.results[0].correct_answer;
+      const [result] = response.data.results;
 
-        // Shuffle the choices and include the correct answer
+      if (result) {
+        const { question: fetchedQuestion, incorrect_answers: fetchedChoices, correct_answer: correct } = result;
         const shuffledChoices = [...fetchedChoices, correct].sort(() => Math.random() - 0.5);
 
-        setQuestion(fetchedQuestion);
+        setQuestion(he.decode(fetchedQuestion));
         setChoices(shuffledChoices);
         setCorrectAnswer(correct);
-
-        // Reset userAnswer and feedback when a new question is fetched
         setUserAnswer(null);
         setFeedback('');
         setShowNextButton(false);
@@ -44,99 +51,102 @@ const Trivia = () => {
   };
 
   useEffect(() => {
-    fetchQuestion(); // Fetch the initial question when the component mounts
-  }, [difficulty]); // Listen for changes in difficulty level
+    fetchQuestion();
+  }, [difficulty, category]);
 
   const handleAnswer = (answer) => {
+    setButtonsDisabled(true); // Disable buttons after selecting an answer
     const isCorrect = answer === correctAnswer;
 
     if (isCorrect) {
       setFeedback('Correct!');
-      setShowNextButton(true); // Show the "Next" button when the answer is correct
-      setCorrectAnswersCount(correctAnswersCount + 1); // Increment correct answers count
+      setShowNextButton(true);
+      setPoints(points + 1);
     } else {
-      setFeedback(`Incorrect. The correct answer is ${correctAnswer}.`); // Provide the correct answer
-      setShowNextButton(false); // Hide the "Next" button for incorrect answers
+      setFeedback(`Incorrect. Correct answer: ${correctAnswer}`);
+      setLives(lives - 1);
+      setTimeout(fetchQuestion, 3000); // Wait for 3 seconds before fetching the next question
     }
 
     setUserAnswer(answer);
   };
 
   const handleNextQuestion = () => {
-    fetchQuestion(); // Fetch the next question
+    fetchQuestion();
   };
 
   const handleRestart = () => {
-    fetchQuestion(); // Fetch the initial question to restart the game
-    setCorrectAnswersCount(0); // Reset correct answers count on restart
-  };
-
-  const renderCongratulations = () => {
-    if (correctAnswersCount === 5) {
-      return (
-        <div className="congratulations">
-          <h2>Congratulations!</h2>
-          <p>You've answered 5 questions correctly.</p>
-          <p>You're a trivia master!</p>
-        </div>
-      );
-    }
-    return null;
+    fetchQuestion();
+    setDifficulty('easy');
+    setUserAnswer(null);
+    setFeedback('');
+    setShowNextButton(false);
+    setPoints(0);
+    setLives(3);
   };
 
   return (
-    <div className="container">
-      <h1>Trivia Game</h1>
-      {renderCongratulations()}
-      <p className="question">Question: {question}</p>
-
-      <div className="choices">
-        {choices.map((choice, index) => (
-          <button key={index} onClick={() => handleAnswer(choice)}>
-            {choice}
-          </button>
-        ))}
+    <div className="main-container">
+      <div className="question-container">
+        <h1>Trivia Game</h1>
+        <p className="question">Question: {question}</p>
+        <div className="choices">
+          {choices.map((choice, index) => (
+            <button
+              key={index}
+              onClick={() => handleAnswer(choice)}
+              disabled={buttonsDisabled} // Disable buttons based on state
+            >
+              {choice}
+            </button>
+          ))}
+        </div>
+        {feedback && (
+          <p className={userAnswer === correctAnswer ? 'feedback success' : 'feedback'}>{feedback}</p>
+        )}
+        {showNextButton && (
+          <div className="button-container">
+            <button className="next" onClick={handleNextQuestion}>Next</button>
+          </div>
+        )}
+        {!showNextButton && userAnswer !== null && lives === 0 && (
+          <div className="button-container">
+            <button className="restart" onClick={handleRestart}>Restart</button>
+          </div>
+        )}
       </div>
-
-      {feedback && (
-        <p className={userAnswer === correctAnswer ? 'feedback success' : 'feedback'}>
-          {feedback}
-        </p>
-      )}
-
-      {showNextButton && (
-        <div className="button-container">
-          <button className="next" onClick={handleNextQuestion}>
-            Next
-          </button>
+      <div className="settings-container">
+        <h2>Game Settings</h2>
+        <div className="setting">
+          <label htmlFor="difficulty">Difficulty:</label>
+          <select
+            id="difficulty"
+            name="difficulty"
+            value={difficulty}
+            onChange={(e) => setDifficulty(e.target.value)}
+          >
+            {difficultyOptions.map((option) => (
+              <option key={option} value={option}>{option}</option>
+            ))}
+          </select>
         </div>
-      )}
-      {!showNextButton && userAnswer !== null && (
-        <div className="button-container">
-          <button className="restart" onClick={handleRestart}>
-            Restart
-          </button>
+        <div className="setting">
+          <label htmlFor="category">Category:</label>
+          <select
+            id="category"
+            name="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            {categoryOptions.map((option) => (
+              <option key={option.id} value={option.id}>{option.name}</option>
+            ))}
+          </select>
         </div>
-      )}
-      <div className="difficulty-buttons">
-        <button
-          className={`difficulty-button easy-button ${difficulty === 'easy' ? 'active' : ''}`}
-          onClick={() => setDifficulty('easy')}
-        >
-          Easy
-        </button>
-        <button
-          className={`difficulty-button medium-button ${difficulty === 'medium' ? 'active' : ''}`}
-          onClick={() => setDifficulty('medium')}
-        >
-          Medium
-        </button>
-        <button
-          className={`difficulty-button hard-button ${difficulty === 'hard' ? 'active' : ''}`}
-          onClick={() => setDifficulty('hard')}
-        >
-          Hard
-        </button>
+        <div className="setting">
+          <p>Lives: {lives}</p>
+          <p>Points: {points}</p>
+        </div>
       </div>
     </div>
   );
